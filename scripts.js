fbdaf12956d843cc520110d15daba935430fc030
Yash Dhiraj Oza <144452060+YashDhirajOza@users.noreply.github.com>
2024-08-30 15:49:18 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x2f2242,
+    points: 12.00,
+    maxDistance: 24.00,
+    spacing: 13.00
+  });
+  
