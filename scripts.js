5a28018f37aa11ed91c70d7d3c3ad9fdf61c9084
Yash Dhiraj Oza <144452060+YashDhirajOza@users.noreply.github.com>
2024-08-30 17:23:20 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,25 @@
-VANTA.NET({
-    el: "#vanta-bg",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    backgroundColor: 0x2f2242,
-    points: 12.00,
-    maxDistance: 24.00,
-    spacing: 13.00
-  });
-  
+let vantaEffect = VANTA.TRUNK({
+  el: "#vanta-bg", 
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  chaos: 3.0, 
+  spacing: 0.5
+});
+let chaosDirection = 0.05; 
+let maxChaos = 5.0; 
+let minChaos = 2.0; 
+
+function animateChaos() {
+  vantaEffect.options.chaos += chaosDirection;
+  if (vantaEffect.options.chaos >= maxChaos || vantaEffect.options.chaos <= minChaos) {
+    chaosDirection *= -1;
+  }
+
+  requestAnimationFrame(animateChaos); 
+}
+animateChaos();
